367154f086db6732f5ca663060e27066a2920a5c
Ben Murdock <159577141+Benmurdock01@users.noreply.github.com>
2025-12-06 15:38:24 -0700

    adding the submission file

--- WhatsCooking.R
+++ WhatsCooking.R
@@ -1,22 +1,63 @@
 library(jsonlite)
-library(dplyr)
-library(purrr)  
-library(stringr)
+library(tidyverse)
+library(tidymodels)
+library(textrecipes)
+library(ranger)
 
-train <- fromJSON('./train.json')
-test <- fromJSON('./test.json')
+#import data
+train_raw <- fromJSON("./train.json") %>% as_tibble()
+test_raw  <- fromJSON("./test.json") %>% as_tibble()
 
-train_features <- train %>%
-  mutate(
-    #Number of Ingredients
-    n_ingredients = map_int(ingredients, length),
-    
-    #Average Ingredient Name Length
-    avg_ing_len = map_dbl(ingredients, ~ mean(nchar(.x))),
-    
-    #Soy Flag
-    has_soy = map_int(ingredients, ~ as.integer(any(str_detect(.x, "soy"))))
-  )
+#Preprocessing
+train_data <- train_raw %>%
+  mutate(ingredients_text = map_chr(ingredients, paste, collapse = " ")) %>%
+  select(id, cuisine, ingredients_text)
 
-# 3. Preview the new features
-head(train_features %>% select(cuisine, n_ingredients, avg_ing_len, has_soy))
+test_data <- test_raw %>%
+  mutate(ingredients_text = map_chr(ingredients, paste, collapse = " ")) %>%
+  select(id, ingredients_text)
+
+#Recipe
+cuisine_recipe <- recipe(cuisine ~ ingredients_text, data = train_data) %>%
+  
+  #Split text into individual ingredients
+  step_tokenize(ingredients_text) %>%
+  
+  #Keep the top 1000 most frequent ingredients
+  step_tokenfilter(ingredients_text, max_tokens = 1000) %>%
+  
+  #TFIDF
+  step_tfidf(ingredients_text) %>%
+  
+  #Normalize
+  step_normalize(all_numeric_predictors())
+
+#Random Forest Model
+rf_spec <- rand_forest(
+  trees = 500,
+  min_n = 5
+) %>%
+  set_engine("ranger",
+             importance = "impurity") %>%
+  set_mode("classification")
+
+#Workflow
+cuisine_wf <- workflow() %>%
+  add_recipe(cuisine_recipe) %>%
+  add_model(rf_spec)
+
+#Fit Model
+final_fit <- cuisine_wf %>%
+  fit(data = train_data)
+
+#Predictions
+predictions <- predict(final_fit, new_data = test_data)
+
+#Submission
+submission <- tibble(
+  id = test_data$id,
+  cuisine = predictions$.pred_class
+)
+
+#Save to CSV
+write_csv(submission, "submission_cuisine_rf.csv")
